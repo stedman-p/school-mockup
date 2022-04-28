a667a479280939e653ed6f40e0f386855801bbef
Peter Stedman <peterfstedman@gmail.com>
2022-04-27 20:58:04 -0400

    still broken *_*

--- api/main.py
+++ api/main.py
@@ -1,21 +1,9 @@
-from urllib import request
-from flask import Flask
+# from urllib import request
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
-
-# students = {
-#     "125122":{
-#         "first_name":"Peter",
-#         "last_name":"Stedman",
-#         "sex":"m"
-#     },
-#     "123456":{
-#         "first_name":"John",
-#         "last_name":"Doe",
-#         "Sex":"m"
-#     }}
 
 student_put_args = reqparse.RequestParser()
 student_put_args.add_argument("student_id",type=int,help="Student id# (6 digits)")
@@ -26,18 +14,19 @@ students = {}
 
 class students(Resource):
     # def get(self,student_id):
-    def get(self):
-        return students
+    def get(self, student_id):
+        return {student_id: students[student_id]}
 
     def put(self,student_id):
-        args = student_put_args.parse_args()
-        return {student_id: args}
+        # args = student_put_args.parse_args()
+        students[student_id] = request.form['data']
+        return {student_id: students[student_id]}
 
 
 
 
-api.add_resource(students, "/students/<int:student_id>")
+api.add_resource(students, "/students/<string:student_id>")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=4000,debug=True)
 
